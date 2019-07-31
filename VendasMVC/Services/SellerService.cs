8e10fb6f34aaa1b4f4788c799b25523db6cb7fa6
Willian Braga <willian.kaeru@gmail.com>
2019-07-31 13:17:39 -0300

    FK not null (ref. integrity)

--- VendasMVC/Services/SellerService.cs
+++ VendasMVC/Services/SellerService.cs
@@ -21,6 +21,7 @@ namespace VendasMVC.Services
         }
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
