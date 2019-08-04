d30efeeb4873f014935407a74131fbe225c23488
Willian Braga <willian.kaeru@gmail.com>
2019-08-04 16:16:58 -0300

    Exception for delete Sallers (Referential Integrity)

---
+++ VendasMVC/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace VendasMVC.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException (string message) : base(message)
+        { 
+        }
+    }
+}
